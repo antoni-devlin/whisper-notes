b173bb0a9e527390ed9c714fec640924f9fbb9d6
Antoni <antoni-devlin@users.noreply.github.com>
2023-10-16 09:43:24 +0000

    wip (uploading works!!!)

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ var express = require("express");
 var app = express();
 const path = require("path");
 
+// RUNNING ORDER
+// 1. Browser POSTs a file to an express API
+// 2. Express saves the file to the disk, ideally in a temporary folder
+// 3. Express API runs `child_process.exec` for the command `python myscript.py /path/to/filename`
+// 4. Express app loads file from disk based on the CLI argument, and when it's done, it writes the result to stdout in JSON
+// 5. The python script finishes and node calls the callback to the exec call and passes in the output string from the python file
+// 6. Express parses the JSON, sends the data back.
+// 7. Express deletes the temp file
+
 // Decode Form URL Encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Require the upload middleware
+const upload = require("./upload");
+
+// Set up a route for file uploads
+app.post("/upload", upload.single("fileUpload"), (req, res) => {
+  // Handle the uploaded file
+  res.json({ message: "File uploaded successfully!" });
+});
+
+// function runTranscription() {}
 
 // Transcribe post route
 app.post("/transcribe", (req, res, next) => {
